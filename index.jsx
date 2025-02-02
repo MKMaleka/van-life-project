ca0f05a03ae4f3089955918d028735af29eed531
MKMaleka <maxwellkagiso@gmail.com>
2025-02-02 19:09:21 +0200

    fixed the cosmetic error

--- index.jsx
+++ index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import About from "./pages/Vans"
+import Vans from "./pages/Vans"
 
 import "./server"
 
